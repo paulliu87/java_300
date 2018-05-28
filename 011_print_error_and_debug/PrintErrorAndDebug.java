96e616a7970307ded5aed728c4252b4300f6bed0
paulliu87 <paul.pliu87@gmail.com>
2018-05-28 14:51:20 -0700

    init and compile java files

---
+++ 011_print_error_and_debug/PrintErrorAndDebug.java
@@ -0,0 +1,12 @@
+public class PrintErrorAndDebug {
+    public static void main(String[] args) {
+        System.out.println("main() starts to work!");
+
+        // manually print some error messages
+        System.err.println("Something went wrong!");
+        System.err.println("\t Please make sure system processes safely.");
+
+        System.out.println("PrintErrorAndDeubg.main()");
+        System.out.println("End of the main()!");
+    }
+}
